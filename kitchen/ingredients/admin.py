70cc396d56a80c1af5aa15fc7e963b1be9519fb5
Robert Grubba <rgrubba@gmail.com>
2022-05-18 07:46:34 +0000

    customized admin for ingredients

--- kitchen/ingredients/admin.py
+++ kitchen/ingredients/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Ingredient,Type
 # Register your models here.
 
-admin.site.register(Ingredient)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name','type','draft')
+    search_fields = ['name']
+    list_filter = ['type','draft']
+
+admin.site.register(Ingredient,IngredientAdmin)
 admin.site.register(Type)
