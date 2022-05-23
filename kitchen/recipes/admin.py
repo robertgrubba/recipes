1c88f0498ba5a4fbde2d96fe278bcad6dc962d8c
Robert Grubba <rgrubba@gmail.com>
2022-05-23 07:02:25 +0000

    improved admin page

--- kitchen/recipes/admin.py
+++ kitchen/recipes/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import Ingredient,Category,Recipe,Step
 
-
-#class PlaceInstanceInline(admin.TabularInline):
-#    model = Place.persons.through
-#    extra = 1
-
 class CategoryInstanceInline(admin.TabularInline):
     model = Category
     extra = 1
@@ -30,13 +25,9 @@ class RecipeInstanceInline(admin.TabularInline):
 
 class RecipeAdmin(admin.ModelAdmin):
     search_fields = ['name']
-    #inlines = [CategoryInstanceInline,StepInstanceInline,IngredientInstanceInline]
     inlines = [StepInstanceInline,IngredientInstanceInline]
     list_filter = ['category','draft'] 
     list_display = ('name','category','draft') 
 
-# Register your models here.
-admin.site.register(Ingredient)
 admin.site.register(Category)
 admin.site.register(Recipe,RecipeAdmin)
-admin.site.register(Step)
