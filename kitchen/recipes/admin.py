3707ce0d7a49f635e112391bf899254dddff6054
Robert Grubba <rgrubba@gmail.com>
2022-05-23 07:13:31 +0000

    ingredients/models.py

--- kitchen/recipes/admin.py
+++ kitchen/recipes/admin.py
@@ -23,11 +23,31 @@ class RecipeInstanceInline(admin.TabularInline):
 #        search_fields = ['name','surname','nickname']
 #        inlines = [PlaceInstanceInline,TelephoneInstanceInline,VehicleInstanceInline,WebpageInstanceInline,FileInstanceInline,NoteInstanceInline]
 
+
+def remove_from_fieldsets(fieldsets, fields):
+    for fieldset in fieldsets:
+        for field in fields:
+            if field in fieldset[1]['fields']:
+                new_fields = []
+                for new_field in fieldset[1]['fields']:
+                    if not new_field in fields:
+                        new_fields.append(new_field)
+                        
+                fieldset[1]['fields'] = tuple(new_fields)
+                break
+
 class RecipeAdmin(admin.ModelAdmin):
     search_fields = ['name']
     inlines = [StepInstanceInline,IngredientInstanceInline]
     list_filter = ['category','draft'] 
     list_display = ('name','category','draft') 
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(RecipeAdmin, self).get_fieldsets(request, obj)
+	
+        if not request.user.is_superuser and request.user.groups.filter(name='publisher').count() == 0:
+            remove_from_fieldsets(fieldsets, ('draft',))
+        return fieldsets
 
 admin.site.register(Category)
 admin.site.register(Recipe,RecipeAdmin)
